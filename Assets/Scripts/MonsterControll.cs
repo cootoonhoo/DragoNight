e0006348b351f30e8810c20650770a59a1e6755c
Marco Antonio <cootoonhoo@gmail.com>
2018-08-14 10:53:20 -0300

    Correcao do paralax e adicao de monstro (aula 8 em andamento)

---
+++ Assets/Scripts/MonsterControll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterControll : MonoBehaviour {
+	public float speed = 2f;
+	public float turnTime = 1f;
+	Rigidbody2D rb;
+	SpriteRenderer sr;
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		sr = GetComponent<SpriteRenderer>();
+	}
+		void Update () {
+			Move();
+	}
+	void Move(){
+		rb.velocity = new Vector2(speed, rb.velocity.y);
+	}
+}
