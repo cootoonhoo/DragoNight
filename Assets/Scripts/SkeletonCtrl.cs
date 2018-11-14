7fa9572804a0f711095ccc683aeba3946720ee86
cootoonhoo <cootoonhoo@gmail.com>
2018-11-14 09:04:29 -0200

    Implementação do inimigo voador(quase uma bala)

---
+++ Assets/Scripts/SkeletonCtrl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonCtrl : MonoBehaviour {
+	public float speed = 10f;
+	public Vector2 direction = new Vector2(-1f, 0f);
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+	void Move(){
+		rb.velocity = speed*direction.normalized;
+	}
+}
